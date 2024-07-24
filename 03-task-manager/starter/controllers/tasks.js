e0062706f0f4d014f120dc4e516f14f74037b756
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-07-24 10:01:05 -0400

    week6 homework assignment

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,35 +1,57 @@
-const Task = require('../models/Task')
-
+const Task = require("../models/Task");
+const asyncWrapper = require('../middleware/async')
+const {createCustomError} = require('../errors/custom-errors')
 // app.get('/api/v1/tasks')
-const getAllTasks = (req,res) => {
-    res.send('get all tasks')
-}
+
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
 
 // app.post('/api/v1/tasks')
-const createTask = async (req,res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
-}
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
 
 // app.get('/api/v1/tasks/:id')
-const getTask = (req,res) => {
-    res.json({id: req.params.id})
-}
-
-// app.patch('/api/v1/tasks/:id')
-const updateTask = (req,res) => {
-    res.send('updating tasks')
-}
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
+  if (!task) {
+    return next(createCustomError(`${taskID} not found`, 404))
+  }
+  res.status(200).json({ task });
+});
 
 // app.delete('/api/v1/tasks/:id')
-const deleteTask = (req,res) => {
-    res.send('deleting tasks')
-}
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    return next(createCustomError(`${taskID} not found`, 404))
+  }
+  res.status(200).json({ task, msg: `${taskID} has been deleted` });
+});
 
-module.exports = { 
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask,
-}
+// app.patch('/api/v1/tasks/:id')
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  }); 
+  if (!task) {
+    return next(createCustomError(`${taskID} not found`, 404))
+  }
+  res.status(200).json({ task });
+});
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose')
 
 
 const TaskSchema = new mongoose.Schema({
-    name:String,completed:Boolean
+    name:{ 
+    type:String, 
+    required:[true,"must provide a name"], 
+    trim:true,
+    maxlength:[20, "Name can not exceed 20 characters" ],
+    },
+    completed:{
+        type:Boolean,
+        default:false,
+    },
 })
 
-module.exports = mongoose.model('Task', TaskSchema)
+module.exports = mongoose.model('Task', TaskSchema) 
