957be167931aaa67a527023cd2564b5baf626e6d
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-08-02 13:47:48 -0400

    week7 homework

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,40 @@
-console.log('04 Store API')
+require('dotenv').config()
+require('express-async-errors')
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products')
+
+const notFoundMiddleWare = require('./middleware/not-found')
+const errorMiddleWare = require('./middleware/error-handler')
+
+//middleWare
+app.use(express.json())
+
+// routes
+app.get('/', (req,res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+app.use('/api/v1/products', productsRouter)
+
+// products route
+
+app.use(notFoundMiddleWare)
+app.use(errorMiddleWare)
+
+const port =  process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        // conenct db
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
