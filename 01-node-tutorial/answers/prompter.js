8c10ee6cfba64283ae0994b2d42ed4910827e299
Tristan White <113216091+up2code3@users.noreply.github.com>
2025-04-02 19:25:52 -0400

    week 2

--- 01-node-tutorial/answers/prompter.js
+++ 01-node-tutorial/answers/prompter.js
@@ -60,5 +60,9 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000);
+server.on("request", (req) => {  
+  console.log("event received: ");  
+});  
+
+server.listen(3000); 
 console.log("The server is listening on port 3000.");
